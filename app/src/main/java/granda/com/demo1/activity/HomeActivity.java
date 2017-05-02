0d112813822c31bd44b6aa088ac4ea058f3c4edc
granda <grandachn@gmail.com>
2017-05-02 13:17:21 +0800

    重构布局

--- app/src/main/java/granda/com/demo1/activity/HomeActivity.java
+++ app/src/main/java/granda/com/demo1/activity/HomeActivity.java
@@ -23,6 +23,7 @@ public class HomeActivity extends AppCompatActivity
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
         initUI();
         initData();
         addEvent();
